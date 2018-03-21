be718df2b6012ba1c177cf7861c8c613e4f9e596
Neivit <neivitweb@gmail.com>
2018-03-20 22:14:28 -0400

    Commit message

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id:1 , name: 'Yocsan Burgos' },
+	{ id:2 , name: 'Simon Bolivar' },
+	{ id:3 , name: 'Wiston Churchill' },
+	{ id:4 , name: 'Abraham Lilncon' },
+	{ id:5 , name: 'Nelson Mandela' },
+	{ id:6 , name: 'Elon Musk' },
+	{ id:7 , name: 'Steve Jobs' },
+	{ id:8 , name: 'Leonardo Da Vinci' },
+	{ id:9 , name: 'Albert Einstein' },
+	{ id:10 , name: 'Barack Obama' },
+	{ id:11 , name: 'Napoleon Bonaparte' },
+	{ id:12 , name: 'Vladimir Putin' }
+];
